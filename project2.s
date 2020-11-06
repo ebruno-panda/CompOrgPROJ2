c852c2fa8f135427d702ab94303b44b843d44d0a
ebruno-panda <ebruno@DESKTOP-5KT47FQ.localdomain>
2020-11-06 12:07:05 -0500

    ASCII upper case boolean value registers

--- project2.s
+++ project2.s
@@ -6,4 +6,8 @@
 .text
 	main:
 		li $s1, 0x61 # $s1 = 'a' 
-		li $s2, 0x74 # $s2 = 't' 
+		li $s2, 0x74 # $s2 = 't'
+		li $s3, 0x41 # $s3 = 'A' 
+		li $s4, 0x54 # $s4 = 'T' 
+		li $s5, 0x30 # $s5 = '0' 
+		li $s6, 0x39 # $s6 = '9' 
